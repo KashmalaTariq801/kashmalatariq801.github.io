c5f65e172afad8a0d97f7d539199cfc56cc04f8b
kashmalatariq801 <k707881172@gmail.com>
2026-09-11 02:25:11 -0700

    fix github pages deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,6 @@
 import { defineConfig } from "astro/config";
-import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
   site: "https://KashmalaTariq801.github.io",
   base: "/umerzai-printing-press-website",
-  vite: {
-    plugins: [tailwindcss()],
-  },
 });
